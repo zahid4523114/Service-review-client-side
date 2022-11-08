105668095b28b027f7db1f1c7b50467e7254ac32
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-11-08 13:26:25 +0600

    first commit

---
+++ src/components/contexts/AuthProvider.js
@@ -0,0 +1,17 @@
+import React, { createContext, useState } from "react";
+import app from "../../firebase/firebase.config";
+import { getAuth } from "firebase/auth";
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = () => {
+  const [user, setUser] = useState({});
+  const [loader, setLoader] = useState(true);
+
+  const authInfo = {};
+
+  return <AuthContext.Provider></AuthContext.Provider>;
+};
+
+export default AuthProvider;
