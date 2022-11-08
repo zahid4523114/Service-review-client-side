ec38ecd1cc281908140359795793a4452b987beb
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-11-08 14:21:55 +0600

    component-library-added

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="#">
+          Navbar
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" href="#">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                Features
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
+                Pricing
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link">Disabled</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Outlet></Outlet>
+    </div>
+  );
 };
 
 export default Main;
