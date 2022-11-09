634ec1b4340e2e921e8907c4615e484902ff94b1
Dewan Mohammod Zahid Hasan <dewanmohammodzahidhasan@gmail.com>
2022-11-09 23:23:56 +0600

    conditional-rendaring-for-review-items

--- src/components/myReview/MyReview.js
+++ src/components/myReview/MyReview.js
@@ -1,8 +1,14 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../contexts/AuthProvider";
 import "./MyReview.css";
+import swal from "sweetalert";
+import UseTitle from "../useTitle/UseTitle";
 
 const MyReview = () => {
+  const alertForData = () => {
+    swal("hello there", "Review deleted successfully", "success");
+  };
+  UseTitle("My Review");
   const { user } = useContext(AuthContext);
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
@@ -26,31 +32,41 @@ const MyReview = () => {
         }
         console.log(data);
       });
-    console.log(id);
   };
   return (
-    <div className="review-container">
-      {reviews.map((review) => (
-        <div key={review._id} class="card my-3" style={{ width: "18rem" }}>
-          <img src={review.image} class="card-img-top" alt="..." />
-          <div class="card-body">
-            <h2>{review.name}</h2>
-            <h4>{review.email}</h4>
-            <p class="card-text">{review.description}</p>
-            <div>
-              <button className="btn rounded-circle px-3 py-2 btn-success">
-                +
-              </button>
-              <button
-                onClick={() => handleReviewDelete(review._id)}
-                className="btn rounded-circle ms-2 px-3 py-2 btn-danger"
-              >
-                -
-              </button>
+    <div>
+      {reviews.length > 0 ? (
+        <div className="review-container">
+          {reviews.map((review) => (
+            <div key={review._id} class="card my-3" style={{ width: "18rem" }}>
+              <img src={review.image} class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h2>{review.name}</h2>
+                <h4>{review.email}</h4>
+                <p class="card-text">{review.description}</p>
+                <div>
+                  <button
+                    onClick={alertForData}
+                    className="btn rounded-circle px-3 py-2 btn-success"
+                  >
+                    +
+                  </button>
+                  <button
+                    onClick={() => handleReviewDelete(review._id)}
+                    className="btn rounded-circle ms-2 px-3 py-2 btn-danger"
+                  >
+                    -
+                  </button>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
+      ) : (
+        <h1 className="text-danger w-100 my-5 text-center">
+          No reviews were added..!
+        </h1>
+      )}
     </div>
   );
 };
